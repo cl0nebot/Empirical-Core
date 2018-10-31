a7069ec4eafaa68f8588e368554ebf6672e58d6d
Donald McKendrick <ddmckendrick@gmail.com>
2018-10-31 12:27:19 -0400

    WIP scaffolding

--- services/QuillLMS/client/app/bundles/Student/startup/Study/activities.tsx
+++ services/QuillLMS/client/app/bundles/Student/startup/Study/activities.tsx
@@ -14,18 +14,48 @@ class Activities extends React.Component<ActivitiesProps, any> {
   renderCategories(activities: any[]) {
     return activities.map((category) => {
       return (
-        <li>
-          {category.name}
-        </li>
+        <div className="fake-table">
+          <div className="header">
+            <span className="header-text">{category.name}</span>
+            <span className="header-list">
+              <span></span>
+              <span className="header-list-counter">
+                {category.activities.length}
+              </span>
+            </span>
+          </div>
+         
+          {this.renderActivities(category.activities)}
+          
+        </div>
+      )
+    })
+  }
+
+  renderActivities(activities: any[]) { 
+    return activities.map((activity) => {
+      return (
+        <div className='line'>
+          <div className="row-list-beginning pull-left">
+            <div className="activate-tooltip icon-link icon-wrapper icon-green icon-diagnostic"></div>
+            <div className="icons-description-wrapper">
+              <p className="title title-v-centered">{activity.name}</p>
+            </div>
+          </div>
+          <div className="row-list-end">
+            <span></span>
+            <p className="title-v-centered text-right">Play</p>
+          </div>
+        </div>
       )
     })
   }
 
   render() {
     return (
-      <ul>
+      <div>
         {this.renderCategories(this.props.activities)}
-      </ul>
+      </div>
     );
   }
 }
